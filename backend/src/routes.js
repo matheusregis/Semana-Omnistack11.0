31884c9f01d46f475fecf23d9116c3c6c594306c
Matheus Régis <matheustrai@gmail.com>
2020-03-27 16:16:41 -0300

    Create Incidents Routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,32 +1,18 @@
 const express = require('express');
-const crypto = require('crypto');
 
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
-routes.get('/ongs', async (req, res) => {
-  const ongs = await connection('ongs').select('*');
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-  return res.json(ongs);
-});
+routes.get('/profile', ProfileController.index);
 
-routes.post('/ongs', async (req, res) => {
-  
-  const { name, email, whatsapp, city, uf } = req.body;
-
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  })
-
-  return res.json({ id });
-});
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
