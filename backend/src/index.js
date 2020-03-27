84b0bef73ff15a7c1b2a7c0a5700b53cf827fc26
Matheus Régis <matheustrai@gmail.com>
2020-03-27 16:36:40 -0300

    Join tables Ongs and Incidents for list params with front-end need/ Add Cors for page security

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,10 @@
 const express = require ('express');
+const corst = require('conts');
 const routes = require('./routes');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-
 
 app.listen(3333);
