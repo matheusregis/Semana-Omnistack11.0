517ef8e03350eacbecf094e72f5fe119c91232ef
Matheus Régis <matheustrai@gmail.com>
2020-03-28 19:12:08 -0300

    Delete Cases and  update list of cases specify ONG

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -24,6 +24,20 @@ export default function Profile() {
     })
   }, [ongId]);
 
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId,
+        }
+      });
+
+      setIncidents(incidents.filter(incident => incident.id !== id));
+    } catch (err) {
+      alert('Erro ao deletar caso, tente novamente.');
+    }
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -49,7 +63,7 @@ export default function Profile() {
             <strong>VALOR:</strong>
             <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
 
-            <button type="button">
+            <button onClick={() => handleDeleteIncident(incident.id)} type="button">
               <FiTrash2 size={20} color="#A8A8b3" />
             </button>
           </li>
