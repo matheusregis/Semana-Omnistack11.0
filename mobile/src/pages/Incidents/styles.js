db6336d268c40f87ad2b69f0913ad130fc4da29a
Matheus Régis <matheustrai@gmail.com>
2020-03-29 02:41:06 -0300

    Create List of Incidents, Configure Navigation

--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -7,7 +7,7 @@ export default StyleSheet.create({
     paddingHorizontal: 24,
     paddingTop: Constants.statusBarHeight + 20,
   },
-  
+
   header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -36,4 +36,38 @@ export default StyleSheet.create({
     lineHeight: 24,
     color: '#737380',
   },
+
+  incidentList: {
+    marginTop: 32,
+  },
+  incident: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#FFF',
+    marginBottom: 16,
+  },
+
+  incidentProperty: {
+    fontSize: 14,
+    color: '#41414d',
+    fontWeight: 'bold',
+  },
+
+  incidentValue: {
+    marginTop: 8,
+    fontSize: 15,
+    marginBottom: 24,
+    color: '#737380',
+  },
+
+  detailsButton: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  detailsButtonText: {
+    color: '#E02041',
+    fontSize: 15,
+    fontWeight: 'bold'
+  }
 });
